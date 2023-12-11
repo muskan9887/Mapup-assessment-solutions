d93b5734c2f394410f6779749b7f02474743fe13
muskan9887 <143822901+muskan9887@users.noreply.github.com>
2023-12-11 19:50:29 +0530

    Update python_task_1.py

--- submissions/python_task_1.py
+++ submissions/python_task_1.py
@@ -12,7 +12,7 @@ Original file is located at
 import pandas as pd
 import numpy as np
 
-df1 = pd.read_csv('/content/dataset-1.csv')
+df1 = pd.read_csv('Datasets/dataset-1.csv')
 
 df2 = pd.read_csv('/content/dataset-2.csv')
 
